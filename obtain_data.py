766c1d12bfc7e96e40acb91f4ed92335e7106721
Amit Shalvi Kali <a.shalvikali@gmail.com>
2025-09-07 17:08:29 +0300

    script split to functions

--- obtain_data.py
+++ obtain_data.py
@@ -5,26 +5,33 @@ PATH_TO_DATA = r'.\raw_data\RISK.tsv'
 PATH_TO_METADATA = r'.\raw_data\metadata.txt'
 
 risk_data = pd.read_csv(PATH_TO_DATA, sep='\t')
-meta = pd.read_csv(PATH_TO_METADATA, sep='\t')
+risk_meta = pd.read_csv(PATH_TO_METADATA, sep='\t')
 risk_data_transpose = risk_data.transpose()
 
-processed_data = pd.DataFrame()
+def obtain_data():
+    processed_data = pd.DataFrame()
 
-for idx, row in risk_data_transpose.iterrows():
-    if idx in ['# OTU','taxonomy']:
-        continue
+    for idx, row in risk_data_transpose.iterrows():
+        if idx in ['# OTU','taxonomy']:
+            continue
 
-    metadata = meta[meta.sample_accession == idx]
-    phenotype = metadata.disease.iloc[0]
-    if phenotype not in ['control', 'CD']:
-        continue
+        metadata = risk_meta[risk_meta.sample_accession == idx]
+        phenotype = metadata.disease.iloc[0]
+        if phenotype not in ['control', 'CD']:
+            continue
 
-    data = risk_data[['# OTU', idx]].copy()
-    data[idx] = data[idx] / data[idx].sum()
+        data = risk_data[['# OTU', idx]].copy()
+        data[idx] = data[idx] / data[idx].sum()
 
-    new_row = {'sample id': idx, 'phenotype': phenotype}
-    new_row.update({int(r['# OTU']):r[idx] for _, r in data.iterrows()})
-    processed_data = pd.concat([processed_data, pd.DataFrame([new_row])], ignore_index=True)
+        new_row = {'sample id': idx, 'phenotype': phenotype}
+        new_row.update({int(r['# OTU']):r[idx] for _, r in data.iterrows()})
+        processed_data = pd.concat([processed_data, pd.DataFrame([new_row])], ignore_index=True)
 
-processed_data.to_csv("processed_data.csv")
+    return processed_data
+
+def filter_uncommon_otus(data, should_appear_in=0.1, at_min_abundance=0.01):
+    pass
+
+data = obtain_data()
+data.to_csv("processed_data.csv")
 
