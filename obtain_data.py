e0d76f557b55ec2315a36fc210d12fe5db118d2e
Amit Shalvi Kali <a.shalvikali@gmail.com>
2025-09-08 12:46:29 +0300

    fix

--- obtain_data.py
+++ obtain_data.py
@@ -3,27 +3,29 @@ import pandas as pd
 import numpy as np
 
 
-PATH_TO_DATA = os.path.join('.', 'raw_data','RISK.tsv')
-PATH_TO_METADATA = os.path.join('.','raw_data', 'metadata.txt')
+PATH_TO_RISK_DATA = os.path.join('.', 'raw_data','RISK.tsv')
+PATH_TO_RISK_METADATA = os.path.join('.','raw_data', 'metadata.txt')
 
 
-def obtain_data():
+def obtain_data(data, metadata):
     processed_data = pd.DataFrame()
 
-    for idx, row in risk_data_transpose.iterrows():
+    # iterate over samples
+    for idx, _ in data.transpose().iterrows():
         if idx in ['# OTU','taxonomy']:
             continue
 
-        metadata = risk_meta[risk_meta.sample_accession == idx]
-        phenotype = metadata.disease.iloc[0]
+        meta = metadata[metadata.sample_accession == idx]
+        phenotype = meta.disease.iloc[0]
         if phenotype not in ['control', 'CD']:
             continue
 
-        data = risk_data[['# OTU', idx]].copy()
-        data[idx] = data[idx] / data[idx].sum()
+        # process to relative abundance
+        sample_data = data[['# OTU', idx]].copy()
+        sample_data[idx] = sample_data[idx] / sample_data[idx].sum()
 
         new_row = {'sample id': idx, 'phenotype': phenotype}
-        new_row.update({int(r['# OTU']):r[idx] for _, r in data.iterrows()})
+        new_row.update({int(r['# OTU']):r[idx] for _, r in sample_data.iterrows()})
         processed_data = pd.concat([processed_data, pd.DataFrame([new_row])], ignore_index=True)
 
     return processed_data
@@ -41,11 +43,10 @@ def filter_uncommon_otus(data, should_appear_in=0.1, min_abundance=0.01):
 
 
 if __name__ == "__main__":
-    risk_data = pd.read_csv(PATH_TO_DATA, sep='\t')
-    risk_meta = pd.read_csv(PATH_TO_METADATA, sep='\t')
-    risk_data_transpose = risk_data.transpose()
+    risk_data = pd.read_csv(PATH_TO_RISK_DATA, sep='\t')
+    risk_meta = pd.read_csv(PATH_TO_RISK_METADATA, sep='\t')
 
-    data = obtain_data()
+    data = obtain_data(risk_data, risk_meta)
     data = filter_uncommon_otus(data)
     data.to_csv("data.csv")
 
