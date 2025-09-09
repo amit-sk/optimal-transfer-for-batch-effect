8e4f9c27acec9e6c0b89898ac17f2f8efed14c32
Amit Shalvi Kali <a.shalvikali@gmail.com>
2025-09-09 18:40:40 +0300

    renormalizing data

--- obtain_data.py
+++ obtain_data.py
@@ -13,21 +13,21 @@ def obtain_relative_abundance_data(data, metadata):
     processed_data = pd.DataFrame()
 
     # iterate over samples
-    for idx, _ in data.transpose().iterrows():
-        if idx in ['# OTU','taxonomy']:
+    for sample_id, _ in data.transpose().iterrows():
+        if sample_id in ['# OTU','taxonomy']:
             continue
 
-        sample_meta = metadata[metadata.sample_accession_16S == idx]
+        sample_meta = metadata[metadata.sample_accession_16S == sample_id]
         phenotype = sample_meta.disease.iloc[0]
         if phenotype not in ['control', 'CD']:
             continue
 
         # process to relative abundance
-        sample_data = data[['# OTU', idx]].copy()
-        sample_data[idx] = sample_data[idx] / sample_data[idx].sum()
+        sample_data = data[['# OTU', sample_id]].copy()
+        sample_data[sample_id] = sample_data[sample_id] / sample_data[sample_id].sum()
 
-        new_row = {'sample_id': idx, 'phenotype': phenotype}
-        new_row.update({int(r['# OTU']):r[idx] for _, r in sample_data.iterrows()})
+        new_row = {'sample_id': sample_id, 'phenotype': phenotype}
+        new_row.update({int(r['# OTU']):r[sample_id] for _, r in sample_data.iterrows()})
         processed_data = pd.concat([processed_data, pd.DataFrame([new_row])], ignore_index=True)
 
     return processed_data
@@ -44,6 +44,16 @@ def filter_uncommon_otus(data, should_appear_in=0.05, min_abundance=0.005):
     return data[cols]
 
 
+def renormalize_data(data):
+    data.set_index('sample_id', inplace=True)
+    otus = [c for c in data.columns if type(c) is int]
+    sums = data[otus].sum(axis=1) 
+    data[otus] = data[otus].div(sums, axis=0)
+    # for sample_id, row in data.iterrows():
+    #     s = row.drop('phenotype').sum()
+    #     data[sample_id] = row.drop('phenotype') / s
+
+
 def main():
     risk_data = pd.read_csv(PATH_TO_RISK_DATA, sep='\t')
     risk_meta = pd.read_csv(PATH_TO_RISK_METADATA, sep='\t')
@@ -52,11 +62,15 @@ def main():
 
     risk_processed_data = obtain_relative_abundance_data(risk_data, risk_meta)
     risk_processed_data = filter_uncommon_otus(risk_processed_data)
+    renormalize_data(risk_processed_data)
     risk_processed_data.to_csv("risk_data.csv")
 
     mucosalibd_processed_data = obtain_relative_abundance_data(mucosalibd_data, mucosalibd_meta)
     mucosalibd_processed_data = filter_uncommon_otus(mucosalibd_processed_data)
+    renormalize_data(mucosalibd_processed_data)
     mucosalibd_processed_data.to_csv("mucosalibd_data.csv")
+
+    print("Done.")
 
 
 if __name__ == "__main__":
