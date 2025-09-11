c705d2c05a8176527c3bddc60a91f62c3ff0a30b
Amit Shalvi Kali <a.shalvikali@gmail.com>
2025-09-11 16:02:00 +0300

    creating combined dataset to show transport

--- optimal_transfer.py
+++ optimal_transfer.py
@@ -9,19 +9,24 @@ import distribution_variance
 
 def barycentric_projection(coupling, src_dataset, x_onto_y=True):
     """ Based on code from SCOTv1 """
+    if type(coupling) is not pd.DataFrame:
+        coupling = pd.DataFrame(coupling)
+    if type(src_dataset) is not pd.DataFrame:
+        src_dataset = pd.DataFrame(src_dataset)
+    
     if x_onto_y:
         # Projecting the first domain onto the second domain
-        weights = np.sum(coupling, axis=1)
-        src_aligned = np.matmul(coupling, src_dataset) / weights[:, None]
+        weights = coupling.sum(axis=1)
+        src_aligned = (coupling @ src_dataset) / weights.values[:, None]
     else:
         # Projecting the second domain onto the first domain
-        weights = np.sum(coupling, axis=0)
-        src_aligned = np.matmul(np.transpose(coupling), src_dataset) / weights[:, None]
+        weights = coupling.sum(axis=0)
+        src_aligned = (coupling.T @ src_dataset) / weights.values[:, None]
 
     return src_aligned
 
 
-def main():
+def sanity_check():
     risk_data = pd.read_csv("risk_data.csv")
     risk_otu_data = risk_data[data_utils.get_otu_columns(risk_data)]
     risk_distance_matrix = squareform(pdist(risk_otu_data.values, metric='braycurtis'))
@@ -31,6 +36,7 @@ def main():
     noisy_otu_data = noisy_data[data_utils.get_otu_columns(noisy_data)]
     noisy_distance_matrix = squareform(pdist(noisy_otu_data.values, metric='braycurtis'))
 
+    # create combined data
     risk_data['dataset'] = 'orig'
     risk_data['sample_id'] = risk_data['sample_id'] + '_orig'
     noisy_data['dataset'] = 'noisy'
@@ -40,22 +46,47 @@ def main():
     combined_data.set_index('sample_id', inplace=True)
     indexes = combined_data.index
     pairs = [(indexes.get_loc(i), indexes.get_loc(i.replace('_orig','_noisy'))) for i in indexes if i.endswith('_orig')]
-    distribution_variance.show_variance(combined_data, 'dataset', pcoa_pairs=pairs)
+
+    # show variance before alignment
+    # distribution_variance.show_variance(combined_data, 'dataset', pcoa_pairs=pairs)
     fracs = distribution_variance.calc_domain_avg_FOSCTTM(risk_otu_data.values, noisy_otu_data.values)
+
+    # transport
+    coupling, log = ot.gromov.gromov_wasserstein(risk_distance_matrix, noisy_distance_matrix, verbose=True, log=True)
+    gw_distance = log['gw_dist']
+    print(f'GW distance: {gw_distance}')
+
+    projected = barycentric_projection(coupling, noisy_otu_data, x_onto_y=False)
+    fracs = distribution_variance.calc_domain_avg_FOSCTTM(risk_otu_data.values, projected.values)
+    
+    projected['sample_id'] = noisy_data['sample_id'].str.replace('_noisy','_projected')
+    projected['dataset'] = 'projected'
+    projected['phenotype'] = noisy_data['phenotype']
+
+    # TODO: create combined data with projected data and show variance
+    combined_data = pd.concat([noisy_data, projected])
+    combined_data.set_index('sample_id', inplace=True)
+    indexes = combined_data.index
+    pairs = [(indexes.get_loc(i), indexes.get_loc(i.replace('_noisy','_projected'))) for i in indexes if i.endswith('_noisy')]
+    # distribution_variance.show_variance(combined_data, 'dataset', pcoa_pairs=pairs)
+
+    # how much each projection had moved
+    combined_data = pd.concat([risk_data, noisy_data, projected])
+    combined_data.set_index('sample_id', inplace=True)
+    indexes = combined_data.index
+    pairs = [(indexes.get_loc(i), indexes.get_loc(i.replace('_noisy','_projected'))) for i in indexes if i.endswith('_noisy')]
+    pairs.append([(indexes.get_loc(i), indexes.get_loc(i.replace('_noisy','_orig'))) for i in indexes if i.endswith('_noisy')])
+    distribution_variance.show_variance(combined_data, 'dataset', pcoa_pairs=pairs)
+
+    print("Done.")
+
+def main():
+    sanity_check()
 
     # mucosalibd_data = pd.read_csv("mucosalibd_data.csv")
     # mucosalibd_otu_data = mucosalibd_data[data_utils.get_otu_columns(mucosalibd_data)]
     # mucosalibd_distance_matrix = squareform(pdist(mucosalibd_otu_data.values, metric='braycurtis'))
 
-    coupling, log = ot.gromov.gromov_wasserstein(risk_distance_matrix, noisy_distance_matrix, verbose=True, log=True)
-    gw_distance = log['gw_dist']
-    print(f'GW distance: {gw_distance}')
-
-    projected = barycentric_projection(coupling, noisy_otu_data.values, x_onto_y=False)
-    fracs = distribution_variance.calc_domain_avg_FOSCTTM(risk_otu_data.values, projected)
-    
-    # TODO: create combined data with projected data and show variance
-    # distribution_variance.show_variance(combined_data, 'dataset', pcoa_pairs=pairs)
     pass
 
 
